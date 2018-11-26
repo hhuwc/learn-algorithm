4954f956dd7dace6f9f9ed5f34eadf661156f980
hhuwc <1727330498@qq.com>
2018-11-26 21:45:14 +0800

    堆排序

--- heap/heapsort.js
+++ heap/heapsort.js
@@ -1,22 +1,22 @@
 let utils = require("../utils/randomArray");
 
-Array.prototype.swap = function(n1, n2) {
-  this[n1] = this[n1] + this[n2];
-  this[n2] = this[n1] - this[n2];
-  this[n1] = this[n1] - this[n2];
+Array.prototype.swap = function (n1, n2) {
+  let tmp = this[n1];
+  this[n1] = this[n2];
+  this[n2] = tmp;
 };
-Array.prototype.heapSort = function() {
+Array.prototype.heapSort = function () {
   var n = this.length - 1;
-  // 最后一个非叶子节点 (n -1) >>1
+  // 最后一个非叶子节点 (n -1) >>1 省去奇数偶数判断
   for (var i = (n - 1) >> 1; i >= 0; i--) {
     this.shiftDown(i, n);
   }
   for (var j = n; j > 0; j--) {
     this.swap(j, 0);
-    this.shiftDown(0, j-1);
+    this.shiftDown(0, j - 1);
   }
 };
-Array.prototype.shiftDown = function(index, end) {
+Array.prototype.shiftDown = function (index, end) {
   while (2 * index + 1 <= end) {
     var j = 2 * index + 1;
     //判断 是否有 左右子树 ,并找出 其中 最大的 一颗 子树
@@ -32,12 +32,17 @@ Array.prototype.shiftDown = function(index, end) {
     index = j;
   }
 };
+
+
+let count = 10000000;
+let arr = utils.randomArray(count); //NearlyOrder
+
 let start = new Date().getTime();
-var count = 1000000;
-var arr = utils.randomArray(count); //NearlyOrder
 arr.heapSort();
 let end = new Date().getTime();
-if (utils.isSorted(arr)) {
-  console.log("堆排序时间" + (end - start));
-}
 
+if (utils.isSorted(arr)) {
+  console.log(`堆排序时间 ${end - start} ms`);
+} else {
+  console.log("排序失败！");
+}
